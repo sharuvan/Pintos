6a82744d81bc09fbc4392fbb579a25d41a854319
i2-rayyan <ilyas2.rayyan@live.uwe.ac.uk>
2023-08-22 19:40:04 +0500

    added necessary syscall functions for argument passing

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -4,6 +4,11 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+#define POS0 4
+#define POS1 8
+#define POS2 12
+#define EXIT_ERROR_CODE -1
+
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -13,9 +18,84 @@ syscall_init (void)
 }
 
 
-static void
-syscall_handler (struct intr_frame *f UNUSED)
-{
-  printf ("system call!\n");
+static void syscall_handler (struct intr_frame *frame) {
+  int call = (int) load_stack (frame, 0);
+
+  switch (call)
+    {
+      case SYS_EXIT:
+        sys_exit ((int) load_stack (frame, POS0));
+        break;
+
+      case SYS_WRITE:
+        frame->eax = sys_write ((int) load_stack (frame, POS0),
+            (const void *) load_stack (frame, POS1),
+            (unsigned int) load_stack (frame, POS2));
+        break;
+
+      default:
+        sys_exit (EXIT_ERROR_CODE); 
+        break;
+    }
+}
+
+// get file map from file descriptor
+struct file_map * get_file_map(int fd) {
+  struct thread *ct = thread_current ();
+  struct list_elem *element;
+  struct file_map *fileMap;
+
+  for (element = list_begin (&ct->fileList); element != list_end (&ct->fileList);
+  	element = list_next (element)) {
+    fileMap = list_entry (element, struct file_map, elem);
+    if (fileMap->fd == fd) return fileMap;
+  }
+  return NULL;
+}
+
+// check if valid buffer
+bool valid_buffer(void *buffer, size_t length) {
+  char *buffer_char = (char *)buffer;
+  for (size_t i = 0; i < length; i++)
+    if (!valid_usr_ptr(buffer_char + i)) return false;
+  return true;
+}
+
+// terminate process with exitcode
+void sys_exit (int status) {
+  struct thread *ct = thread_current ();
+  ct->exitcode = status;
   thread_exit ();
 }
+
+
+// buffered file write
+int sys_write (int fd, const void *buffer, unsigned int length) {
+  struct file_map *fileMap;
+  unsigned int len;
+  char *buffer_char;
+
+  if (!valid_buffer ((void *) buffer, length)) sys_exit (EXIT_ERROR_CODE);
+
+  if (fd == STDOUT_FILENO){ 
+    len = length;
+    buffer_char = (char *) buffer;
+
+    while (len > MAXIMUM_BUFFER){
+      putbuf ((const char *) buffer_char, MAXIMUM_BUFFER);
+      len -= MAXIMUM_BUFFER;
+      buffer_char += MAXIMUM_BUFFER;
+    }
+    putbuf ((const char *) buffer_char, len);
+    return length;
+  }
+  fileMap = get_file_map (fd);
+  if (fileMap == NULL) sys_exit (EXIT_ERROR_CODE);
+  sema_down (&sema);
+  int return_val = file_write (fileMap->file, buffer, length);
+  sema_up (&sema);
+
+  return return_val;
+}
+
+
--- userprog/syscall.h
+++ userprog/syscall.h
@@ -1,6 +1,16 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+// map file structures and descriptors
+struct file_map{
+    struct list_elem elem;      /* List element*/
+    int fd;                     /* File descriptor. */
+    struct file *file;          /* File structure. */
+};
+
+bool valid_buffer (void *buffer, size_t length);
+void sys_exit (int status);
+int sys_write (int fd, const void *buffer, unsigned int length);
 void syscall_init (void);
 
 #endif /* userprog/syscall.h */
